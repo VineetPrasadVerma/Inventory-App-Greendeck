3275d3903360f2f0b14e6fbc9647ae929471ebee
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-06-16 05:18:31 +0530

    Add Frontend of the app: Products and filter are rendering

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const connectDB = require('./config/connection')
 const filterRoutes = require('./routes/filterRoutes')
 // const insertData = require('./seedDB')
@@ -9,6 +10,7 @@ const filterRoutes = require('./routes/filterRoutes')
 connectDB()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/', filterRoutes)
 
