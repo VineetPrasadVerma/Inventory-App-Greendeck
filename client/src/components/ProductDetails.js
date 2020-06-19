badb4cc0ebcfb337be7cc79e7aba258367aeef20
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-06-20 02:15:25 +0530

    Code for production

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ProductImage from '../img/product.webp'
 
 const ProductDetails = ({ product }) => {
   return (
     <div className='column col-md-4 col-sm-6'>
       <div className='thumbnail'>
-        <img alt='product_image' src={ProductImage} />
+        <img alt='product_image' src={product.media.standard[0].url} />
 
         <strong className='caption' style={{ textTransform: 'uppercase' }}>
           {product.brand.name}
