cc49bb78a61c4772c048853b711d70b94cd84813
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-06-19 23:07:40 +0530

    Updated code

--- controllers/filterProduct.js
+++ controllers/filterProduct.js
@@ -6,6 +6,7 @@ const getProducts = async (req, res) => {
     console.log('DB here')
     const products = await Product.find()
     console.log('There')
+    console.log(products)
     return res.status(200).json(products)
   } catch (ex) {
     console.log(ex)
